ef1b93285d96361bd1a5c3af72667d18e5381449
Clucas0311 <clucas0311@gmail.com>
2020-06-11 23:47:01 -0400

    Added the bullet file for bullet shootings

--- settings.py
+++ settings.py
@@ -11,3 +11,9 @@ class Settings:
         # Ship settings
         # The initial value of the ship speed is 1.5
         self.ship_speed = 1.5
+
+        # Bullet Settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
